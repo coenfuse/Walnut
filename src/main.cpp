7165e4cd781225b6cb612c9fb460b32886709d4f
Sarthak Sharma <freelancesarthak@gmail.com>
2020-12-23 14:40:09 +0530

    CMT #2.1: Started implementation of progress tracker.
    Basic idea is working. Some tweaking here and there is needed to make it fully functional.
    
    Current idea is:
    Keep the track of current test's average and at the end of the test, store it in a file. Also fetch the average of all the average scores of test prior to current test. Compare the current test with previous averages and show if there's an improvement or not. Currently the only calculation is of the time taken to solve each question.
    Late this will store a far more condensed report.

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,24 @@
 #include <iostream>
 #include<time.h>
 #include<chrono>
+#include<fstream>
 
-// Whole program can be probably reduced into exponentially small size by correct usage of templates.
+// 1. - Fix the progress record function. Do finishing and testing. Release V1.
+// 2. - Break the program into multiple files and store them appropriately.
+// 3. - Restructure using templates if its okay.
+// 3. - Add more features to progress record function.
+// 4. - Add memory feature, the program will keep track of all the questions where
+// the answer wasn't right and will try to throw it next time.
+// 5. - Make prioprietary API for bots. Use polymorphism if you could.
+// 6. - Define bots and implement a small scale ecosystem to run the bots.
 
 class Test {
 
 	static unsigned int player_count;
 	static float global_avg;
 	const static unsigned int section_length;
+
+	using duration_type = std::chrono::milliseconds;
 
 	float m_score;
 
@@ -20,6 +30,40 @@ private:
 	};
 
 	m_stats results[9];
+
+	int prev_rec(int new_val = 0) {
+		// Need excessive cleaning.
+		using namespace std;
+
+		fstream file("data.dat", std::ios::in);
+		if (file) {
+			// File exists
+			int current, counter = 0;
+			long long total = 0;
+
+			fstream fin("data.dat", std::ios::in);
+			while (fin) {
+				fin >> current;
+				total += current;
+				counter++;
+			}
+			fin.close();
+
+			fstream fout("data.dat", std::ios::out | std::ios::app);
+			fout << new_val << std::endl;
+			fout.close();
+
+			return (total / counter);
+		}
+		else {
+			// File didn't existed
+			fstream fout("data.dat", std::ios::out);
+			fout << new_val;
+			fout.close();
+			return 0;
+		}
+	}
+
 private:
 
 	m_stats square_set();
@@ -154,24 +198,31 @@ void Test::report() {
 	std::cout << "Average time spend on each question: " << results[8].avg_duration << " ms (" << (results[8].avg_duration / 1000) << " seconds)" << std::endl;
 
 	// Final Score Calculation
-	unsigned int global_avg_time = 0;
+	unsigned int test_avg_time = 0;
 	unsigned int total_questions = 0;
 	unsigned int total_score = 0;
 
 	for (size_t i = 0; i < 9; i++) {
-		global_avg_time += results[i].avg_duration;
+		test_avg_time += results[i].avg_duration;
 		total_questions += results[i].questions;
 		total_score		+= results[i].score;
 	}
 
 	max_score = 2 * total_questions;
 	percentage = (total_score / max_score) * 100;
-	global_avg_time = global_avg_time / 9;	// Number of sections
+	test_avg_time = test_avg_time / 9;	// Number of sections
+
+	// Update and fetch previous record
+	int prev_avg = 0;
+	prev_avg = prev_rec(test_avg_time);
 
 	std::cout << "\n\nFINAL REPORT CARD" << std::endl;
 	std::cout << "------------------" << std::endl;
 	std::cout << "You scored " << total_score << " out of " << max_score <<"."<< std::endl;
 	std::cout << "Your final percentage is: " << percentage <<" %"<< std::endl;
+	std::cout << "Your average duration on each question is: " << test_avg_time << " ms" << std::endl;
+	std::cout << "Your previous average was: " << prev_avg << std::endl;
+	std::cout << "You have made an improvement of: " << ((prev_avg - test_avg_time)/1000) << " ms"<< std::endl;
 
 	std::cout << "\n\nPress Enter to exit" << std::endl;
 	std::cin.get();
@@ -197,7 +248,7 @@ Test::m_stats Test::square_set() {
 		std::cin >> response;
 		auto end = std::chrono::high_resolution_clock::now();
 		
-		auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+		auto duration = std::chrono::duration_cast<duration_type>(end - start);
 		total_time_taken += duration.count();
 
 		if (response == correct_answer)
@@ -229,7 +280,7 @@ Test::m_stats Test::perfect_root_set() {
 		std::cin >> response;
 		auto end = std::chrono::high_resolution_clock::now();
 		
-		auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+		auto duration = std::chrono::duration_cast<duration_type>(end - start);
 		total_time_taken += duration.count();
 
 		if (response == operand)
@@ -261,7 +312,7 @@ Test::m_stats Test::root_set(){
 		std::cin >> response;
 		auto end = std::chrono::high_resolution_clock::now();
 		
-		auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+		auto duration = std::chrono::duration_cast<duration_type>(end - start);
 		total_time_taken += duration.count();
 
 		if (response == correct_answer)
@@ -295,7 +346,7 @@ Test::m_stats Test::product_set() {
 		std::cin >> response;
 		auto end = std::chrono::high_resolution_clock::now();
 
-		auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+		auto duration = std::chrono::duration_cast<duration_type>(end - start);
 		total_time_taken += duration.count();
 	
 		if (response == correct_answer)
@@ -329,7 +380,7 @@ Test::m_stats Test::sum_set(){
 		std::cin >> response;
 		auto end = std::chrono::high_resolution_clock::now();
 
-		auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+		auto duration = std::chrono::duration_cast<duration_type>(end - start);
 		total_time_taken += duration.count();
 
 		if (response == correct_answer)
@@ -363,7 +414,7 @@ Test::m_stats Test::division_set() {
 		std::cin >> response;
 		auto end = std::chrono::high_resolution_clock::now();
 
-		auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+		auto duration = std::chrono::duration_cast<duration_type>(end - start);
 		total_time_taken += duration.count();
 
 		if (response == correct_answer)
@@ -403,7 +454,7 @@ Test::m_stats Test::difference_set() {
 		std::cin >> response;
 		auto end = std::chrono::high_resolution_clock::now();
 
-		auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+		auto duration = std::chrono::duration_cast<duration_type>(end - start);
 		total_time_taken += duration.count();
 
 		if (response == correct_answer)
@@ -435,7 +486,7 @@ Test::m_stats Test::cube_set() {
 		std::cin >> response;
 		auto end = std::chrono::high_resolution_clock::now();
 
-		auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+		auto duration = std::chrono::duration_cast<duration_type>(end - start);
 		total_time_taken += duration.count();
 
 		if (response == correct_answer)
@@ -467,7 +518,7 @@ Test::m_stats Test::cube_root_set() {
 		std::cin >> response;
 		auto end = std::chrono::high_resolution_clock::now();
 
-		auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+		auto duration = std::chrono::duration_cast<duration_type>(end - start);
 		total_time_taken += duration.count();
 
 		if (response == operand)
@@ -480,14 +531,149 @@ Test::m_stats Test::cube_root_set() {
 }
 
 int main() {
+	
+	Test newTest;
+
+	std::cout << "To start the test, press Enter" << std::endl;
+	std::cin.get();
+
 	auto start = std::chrono::high_resolution_clock::now();
-	auto end = std::chrono::high_resolution_clock::now();
-	auto duration = std::chrono::duration_cast<std::chrono::seconds>(end - start);
-	int time = duration.count();
-	Test newTest; 
 	newTest.start();
+	auto end = std::chrono::high_resolution_clock::now();
+	auto duration = std::chrono::duration_cast<std::chrono::minutes>(end - start);
+	int time = duration.count();
+
 	std::cout << "Press enter to see your result" << std::endl;
 	std::cin.get();
+
+	std::cout << "\n\nTest Duration: " << time << " minutes." << std::endl;
 	newTest.report();
+	
+
 	return 0;
 }
+
+/*
+// A C++ Program to demonstrate the 
+// four file hacks every C/C++ must know 
+
+// Note that we are assuming that the files 
+// are present in the same file as the program 
+// before doing the below four hacks 
+#include<stdio.h> 
+#include<stdlib.h> 
+#include<stdbool.h> 
+
+// A Function to get the file size 
+unsigned long long int fileSize(const char* filename)
+{
+	// Open the file 
+	FILE* fh = fopen(filename, "rb");
+	fseek(fh, 0, SEEK_END);
+	unsigned long long int size = ftell(fh);
+	fclose(fh);
+
+	return (size);
+}
+
+// A Function to check if the file exists or not 
+bool fileExists(const char* fname)
+{
+	FILE* file;
+	if (file = fopen(fname, "r"))
+	{
+		fclose(file);
+		return (true);
+	}
+	return (false);
+}
+
+// Driver Program to test above functions 
+int main()
+{
+	printf("%llu Bytes\n", fileSize("Passwords.txt"));
+	printf("%llu Bytes\n", fileSize("Notes.docx"));
+
+	if (fileExists("OldData.txt") == true)
+		printf("The File exists\n");
+	else
+		printf("The File doen't exist\n");
+
+	rename("Videos", "English_Videos");
+	rename("Songs", "English_Songs");
+
+	remove("OldData.txt");
+	remove("Notes.docx");
+
+	if (fileExists("OldData.txt") == true)
+		printf("The File exists\n");
+	else
+		printf("The File doesn't exist\n");
+
+	return 0;
+}
+
+
+File Handling with C++ using fstream class object 
+To write the Content in File 
+Then to read the content of file
+#include <iostream> 
+
+fstream header file for ifstream, ofstream,
+   fstream classes
+#include <fstream> 
+
+using namespace std;
+
+// Driver Code 
+int main()
+{
+	// Creation of fstream class object 
+	fstream fio;
+
+	string line;
+
+	// by default openmode = ios::in|ios::out mode 
+	// Automatically overwrites the content of file, To append 
+	// the content, open in ios:app 
+	// fio.open("sample.txt", ios::in|ios::out|ios::app) 
+	// ios::trunc mode delete all conetent before open 
+	fio.open("sample.txt", ios::trunc | ios::out | ios::in);
+
+	// Execute a loop If file successfully Opened 
+	while (fio) {
+
+		// Read a Line from standard input 
+		getline(cin, line);
+
+		// Press -1 to exit 
+		if (line == "-1")
+			break;
+
+		// Write line in file 
+		fio << line << endl;
+	}
+
+	// Execute a loop untill EOF (End of File) 
+	// point read pointer at beginning of file 
+	fio.seekg(0, ios::beg);
+
+	while (fio) {
+
+		// Read a Line from File 
+		getline(fio, line);
+
+		// Print line in Console 
+		cout << line << endl;
+	}
+
+	// Close the file 
+	fio.close();
+
+	return 0;
+}
+
+
+
+
+*/
